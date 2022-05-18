6dd0f82f1cdce8a9082c5858db970bf3e91996c2
coji <coji@techtalk.jp>
2022-05-18 23:10:33 +0900

    adjust margin

--- src/app1/pages/ConfirmPage.vue
+++ src/app1/pages/ConfirmPage.vue
@@ -21,17 +21,17 @@ const handleClickNext = () => {
     <AppHeading level="1">以下の内容で予約します</AppHeading>
 
     <div class="grid grid-cols-3 gap-2">
-      <div class="p-3 font-bold text-right bg-gray-100">希望店舗</div>
-      <div class="col-span-2 p-3">{{ selection.shop }}</div>
+      <div class="p-2 font-bold text-right bg-gray-100">希望店舗</div>
+      <div class="col-span-2 p-2">{{ selection.shop }}</div>
 
-      <div class="p-3 font-bold text-right bg-gray-100">人数</div>
-      <div class="col-span-2 p-3">{{ selection.member }}</div>
+      <div class="p-2 font-bold text-right bg-gray-100">人数</div>
+      <div class="col-span-2 p-2">{{ selection.member }}</div>
 
-      <div class="p-3 font-bold text-right bg-gray-100">第1希望日時</div>
-      <div class="col-span-2 p-3">{{ selection.firstChoice }}</div>
+      <div class="p-2 font-bold text-right bg-gray-100">第1希望日時</div>
+      <div class="col-span-2 p-2">{{ selection.firstChoice }}</div>
 
-      <div class="p-3 font-bold text-right bg-gray-100">第2希望日時</div>
-      <div class="col-span-2 p-3">{{ selection.secondChoice }}</div>
+      <div class="p-2 font-bold text-right bg-gray-100">第2希望日時</div>
+      <div class="col-span-2 p-2">{{ selection.secondChoice }}</div>
     </div>
 
     <AppNextButton @click="handleClickNext()"> 予約完了 </AppNextButton>
